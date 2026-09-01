cc933cda7b20619026ac0dfbc63a290620c6b04c
unknown <neilandreiabines13@gmail.com>
2026-09-01 19:05:12 +0800

    sample

---
+++ Assignment3.java
@@ -0,0 +1,34 @@
+// Declares a class named Assignment3
+public class Assignment3 {
+
+    // Starting point of Java Program
+    public static void main(String[] args){
+
+        // Declares a String variable named firstname
+        String firstname = "Neil Andrei";
+        // Declares a String variable named lastname
+        String lastname = "Abines";
+        // Declares a String variable named address
+        String address = "Natividad, Pangasinan";
+        // Declares a String variable named mobileNumber
+        String mobileNumber = "09272390046";
+        // Declares an integer variable named age
+        int age = 18;
+        // Declares a double variable named gwa
+        double gwa = 97.00;
+
+            //Displays the value of the firstname
+            System.out.println("Firstname: " + firstname);
+            //Displays the value of the lastname
+            System.out.println("Lastname: " + lastname);
+            //Displays the value of the address
+            System.out.println("Address: " + address);
+            //Displays the value of the mobileNumber
+            System.out.println("Mobile Number: " + mobileNumber);
+            //Displays the value of the age
+            System.out.println("Age: " + age);
+            //Displays the value of the gwa
+             System.out.println("GWA: " + gwa);
+
+    }
+}
